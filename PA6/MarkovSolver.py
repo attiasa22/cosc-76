12caea9a32b7e5cf0ade6a9f25238a6570813139
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2021-11-11 20:16:35 -0500

    organizing

--- PA6/MarkovSolver.py
+++ PA6/MarkovSolver.py
@@ -1,88 +1,85 @@
 import numpy as np
 from sklearn.preprocessing import normalize
-from random import choice
 from MazeProblem import MazeProblem
 from Maze import Maze
 
 class MarkovSolver:
 
-    def __init__(self, maze_problem):
-        self.maze_problem = maze_problem
+    def __init__(self, problem):
+        self.problem = problem
         
-
-    def filtering(self):
+    def filtering(self, observations):
         # this function continuously applies the matrix transformation version
         # f_1:t+1 = a(O_t+1)(T_transpose)(f_1:t)
-        f_t = self.maze_problem.initial_distribution
+        f_t = self.problem.initial_distribution
         transition_matrix_transpose = self.transition_matrix().T
         # apply the HMM a certain number of times
-        for i in range(50):
+        for i in range(observations):
             observation_matrix = self.observation_matrix()
-            #sci_kit learn's normalize function performs the same action as a in the equation
+            # sci_kit learn's normalize function performs the same action as a in the equation
             f_t = normalize(((observation_matrix @ transition_matrix_transpose) @ f_t).reshape(-1,1), axis=0, norm = 'l1')
-            self.maze_problem.solution += [f_t]
+            self.problem.solution += [f_t]
         
-
     def viterbi(self):
-        color_map = {"R":0, "G":1, "B":2, "Y":3}
-        observations =  self.maze_problem.observed_color
-        print(len(observations))
+        obvservation_space = self.problem.observation_space
+
+        observations =  self.problem.observations
         transition_matrix = self.transition_matrix()
-        print(transition_matrix.shape)
-        emission_matrix = self.emission_matrix()
-        print(emission_matrix.shape)
-        initial_probabilities = self.maze_problem.initial_distribution
+        emission_matrix = self.emission_matrix(obvservation_space)
+        initial_probabilities = self.problem.initial_distribution
         
-        state_size = len(self.maze_problem.list_of_floors)
+        state_size = len(self.problem.states)
         observation_count = len(observations)
+
         T1 = np.zeros((state_size, observation_count))
         T2 = np.zeros((state_size, observation_count))
-        T1[:, 0] = initial_probabilities * emission_matrix[:, color_map[observations[0]]]
+        T1[:, 0] = initial_probabilities * emission_matrix[:, obvservation_space[observations[0]]]
 
         for i in range(1, observation_count):
-
-            T1[:,i] = np.max((T1[:, i-1 ] * transition_matrix.T) * emission_matrix[:, color_map[observations[i]]].T, 1)
+            T1[:,i] = np.max((T1[:, i-1] * transition_matrix.T) * emission_matrix[:, obvservation_space[observations[i]]].T, 1)
             T2[:, i] = np.argmax(T1[:, i-1 ] * transition_matrix.T, 1)
+
         solution = np.zeros(observation_count)
         solution[-1] = np.argmax(T1[:, observation_count - 1])
 
         for i in reversed(range(1,observation_count )):
             solution[i - 1] = int(T2[int(solution[i]), i])
-        print(solution)
-        self.maze_problem.veterbi_solution = solution
+
+        self.problem.veterbi_solution = solution
         return solution
+
     # Given S possible states, an S x S matrix T is created, such that T[i][j] is chance that we go from i to j
     # Since our robot takes a random movement in the 4 cardinal directions, that chance is 0.25
     # However, a floor bordering a wall actually has a chance to come from itself, 
     # So any wall encountered is actually an increase of 0.25 to T[i][i]
     def transition_matrix(self):
         # initialize a S xS matrix of zeros
-        dimension = len(self.maze_problem.list_of_floors)
+        dimension = len(self.problem.states)
         transition_matrix= np.zeros((dimension,dimension))
         # for each state
-        for floor in self.maze_problem.list_of_floors:
-            index_floor_1 = self.maze_problem.list_of_floors.index(floor)
+        for floor in self.problem.states:
+            index = self.problem.states.index(floor)
             x = floor[0] 
             y = floor[1]
             # determine if neighboring cells are possible previous states
-            self.set_matrix(transition_matrix, index_floor_1, x+1, y)
-            self.set_matrix(transition_matrix, index_floor_1, x-1, y)
-            self.set_matrix(transition_matrix, index_floor_1, x, y+1)
-            self.set_matrix(transition_matrix, index_floor_1, x, y-1)
+            self.set_matrix(transition_matrix, index, x+1, y)
+            self.set_matrix(transition_matrix, index, x-1, y)
+            self.set_matrix(transition_matrix, index, x, y+1)
+            self.set_matrix(transition_matrix, index, x, y-1)
 
         return transition_matrix
 
-    def set_matrix(self, transition_matrix, index_floor_1, x, y):
+    def set_matrix(self, transition_matrix, index_1, x, y):
         # if the cell is a state
-        if [x,y] in self.maze_problem.list_of_floors:
+        if [x,y] in self.problem.states:
                 # set T[i][j] to probabilty of transition
-                index_floor_2 = self.maze_problem.list_of_floors.index([x,y])
-                transition_matrix[index_floor_2][index_floor_1] = 0.25
+                index_2 = self.problem.states.index([x,y])
+                transition_matrix[index_2][index_1] = 0.25
         else:
             # if the cell is not a state
             # the movement will leave the robot in the same state
             # meaning that the state is reachable from itself
-            transition_matrix[index_floor_1][index_floor_1] += 0.25
+            transition_matrix[index_1][index_1] += 0.25
 
     # Create a diagonal S x S matrix 
     # where the diagonal values are the odds of that state occuring 
@@ -90,63 +87,43 @@ class MarkovSolver:
     def observation_matrix(self):
         # initialize a 1 x S matrix
         # where each cell i is an observation probability of state i
-        odds = [0]* len(self.maze_problem.list_of_floors)
+        odds = [0]* len(self.problem.states)
         
-        new_color = self.get_new_color()
+        new_observation = self.problem.get_new_state()
         # for each state
-        for index in range(len(self.maze_problem.list_of_floors)):
+        for index in range(len(self.problem.states)):
             # if state is the same as the observed state
-            if self.maze_problem.maze.get_floor(self.maze_problem.list_of_floors[index][0],self.maze_problem.list_of_floors[index][1]) == new_color:
+            x = self.problem.states[index][0]
+            y = self.problem.states[index][1]
+            if self.problem.maze.get_floor(x, y) == new_observation:
                 # add the probability it will be sensed correctly
-                odds[index] = self.maze_problem.color_probability
+                odds[index] = self.problem.color_probability
             else:
-                odds[index] = 1 - self.maze_problem.color_probability
+                odds[index] = 1 - self.problem.color_probability
 
         # O matrix is the S x S matrix whose diagonals are the observation probabilities
         observational_matrix = np.diag(odds)
 
         return observational_matrix
 
-    def emission_matrix(self):
-        color_map = {"R":0, "G":1, "B":2, "Y":3}
-        emission_matrix = np.full((len(self.maze_problem.list_of_floors), 4), 0.12)
+    def emission_matrix(self, obvservation_space):
+        emission_matrix = np.full((len(self.problem.states), len(self.problem.observation_space)), self.problem.color_probability-1)
 
-        for i in range((len(self.maze_problem.list_of_floors))):
-            
-            j= color_map[self.maze_problem.maze.get_floor(self.maze_problem.list_of_floors[i][0],self.maze_problem.list_of_floors[i][1])]
-            emission_matrix[i,j] = self.maze_problem.color_probability
+        for i in range((len(self.problem.states))):
+            x = self.problem.states[i][0]
+            y = self.problem.states[i][1]
+            j = obvservation_space[self.problem.maze.get_floor(x,y)]
+            emission_matrix[i, j] = self.problem.color_probability
 
         return emission_matrix
-
-    def get_new_color(self):
-        # get current state coordinates
-        x = self.maze_problem.current_state[0]
-        y = self.maze_problem.current_state[1]
-
-        # randomly choose movement to new state
-        new_state = choice([[x+1,y],[x-1,y],[x,y+1],[x,y-1]])
-        
-        if not self.maze_problem.maze.is_floor(new_state[0],new_state[1]):
-            new_state = self.maze_problem.current_state
-        chosen_floor_color = self.maze_problem.maze.get_floor(new_state[0],new_state[1])
-        other_colors = set(["R","G","B","Y"]).difference(set(chosen_floor_color))
-
-        inverse_probability = (1-self.maze_problem.color_probability)/3
-        p_distribution = [self.maze_problem.color_probability,inverse_probability,inverse_probability,inverse_probability]
-        new_color = np.random.choice([chosen_floor_color]+list(other_colors),p=p_distribution)
-
-        self.maze_problem.true_states += [new_state]
-        self.maze_problem.observed_color += [new_color]
-        
-        return new_color
 
 if __name__ == "__main__":
 
     test_maze1 = Maze("PA6/maze1.maz")
-    problem = MazeProblem(test_maze1)
+    problem = MazeProblem(test_maze1, 0.88)
     solver = MarkovSolver(problem)
     
-    solver.filtering()
-    solver.viterbi()
+    solver.filtering(20)
+    # uncomment for viterbi
+    # solver.viterbi()
     problem.show_solution()
-    
--- PA6/MazeProblem.py
+++ PA6/MazeProblem.py
@@ -5,23 +5,23 @@ import numpy as np
 
 class MazeProblem:
 
-    def __init__(self, maze):
+    def __init__(self, maze, true_positive):
         self.maze = maze
         # list of all possible floors - the states used by the markov problem
-        self.list_of_floors = maze.find_floors()
+        self.states = maze.find_floors()
         # each floor is evenly likely at the start
         self.initial_distribution = self.initialize_matrix()
         # set a random initial state for the robot
         self.current_state = self.set_random_start() 
         # odds sensor is correct
-        self.color_probability = 0.88
+        self.color_probability = true_positive
         # list of distributions
         self.solution = [self.initial_distribution]
         self.veterbi_solution = []
         # robot's path
         self.true_states = [self.current_state]
-
-        self.observed_color = [str(self.maze.get_floor( self.current_state[0],self.current_state[1]))]
+        self.observation_space = {"R":0, "G":1, "B":2, "Y":3}
+        self.observations = [str(self.maze.get_floor( self.current_state[0],self.current_state[1]))]
 
 
     def __str__(self):
@@ -29,40 +29,63 @@ class MazeProblem:
         return string
 
     def initialize_matrix(self):
-        initial_distribution = 1/len(self.list_of_floors)
-        distribution_matrix = np.array([initial_distribution]*len(self.list_of_floors))
+        initial_distribution = 1/len(self.states)
+        distribution_matrix = np.array([initial_distribution]*len(self.states))
 
         return distribution_matrix
 
     def set_random_start(self): 
-        if len(self.list_of_floors)==0 or len(self.list_of_floors)==1: 
+        if len(self.states)==0 or len(self.states)==1: 
             print("No HMM necessary")
         
-        return choice(self.list_of_floors)
+        return choice(self.states)
+
+    def get_new_state(self):
+        # get current state coordinates
+        x = self.current_state[0]
+        y = self.current_state[1]
+
+        # randomly choose movement to new state
+        new_state = choice([[x+1,y], [x-1,y], [x,y+1], [x,y-1]])
+        
+        if not self.maze.is_floor(new_state[0], new_state[1]):
+            new_state = self.current_state
+        chosen_floor_color = self.maze.get_floor(new_state[0], new_state[1])
+        other_colors = set(["R", "G", "B", "Y"]).difference(set(chosen_floor_color))
+
+        inverse_p = (1-self.color_probability)/3
+        p_distribution = [self.color_probability,inverse_p,inverse_p,inverse_p]
+        new_color = np.random.choice([chosen_floor_color]+list(other_colors),p = p_distribution)
+
+        self.true_states += [new_state]
+        self.observations += [new_color]
+        
+        return new_color
 
     def show_solution(self):
-        print(len(self.solution))
-        print(len(self.veterbi_solution))
         for i in range(len(self.solution)):
+
             probability_matrix = np.zeros((self.maze.height,self.maze.width))
+
             self.maze.robotloc[0] = self.true_states[i][0]
             self.maze.robotloc[1] = self.true_states[i][1]
             self.maze.create_render_list()
+
             print("Current Position: " + str(self.maze.robotloc))
-            print("Veterbi Position: " + str(self.list_of_floors[int(self.veterbi_solution[i])]))
-            print("Percieved Color: " + str(self.observed_color[i]))
+            if len(self.veterbi_solution):
+                print("Veterbi Position: " + str(self.states[int(self.veterbi_solution[i])]))
+            print("Percieved Color: " + str(self.observations[i]))
             print("Real Color: " + str(self.maze.get_floor( self.maze.robotloc[0],self.maze.robotloc[1])))
             print("MAZE: \n"+str(self.maze))
 
             for j in range(len(self.solution[i])):
-                probability_matrix[self.list_of_floors[j][1]][self.list_of_floors[j][0]] = self.solution[i][j]
+                probability_matrix[self.states[j][1]][self.states[j][0]] = self.solution[i][j]
             print(np.flip(probability_matrix, 0))
 
-
-
-
-
-if __name__ == "__main__":
-    test_maze1 = Maze("PA6/maze1.maz")
-    problem = MazeProblem(test_maze1)
-    print(problem.initialize_matrix())
+# used for testing
+#
+# if __name__ == "__main__":
+#     test_maze1 = Maze("PA6/maze1.maz")
+#     problem = MazeProblem(test_maze1)
+#     print(problem.initialize_matrix())
+#
